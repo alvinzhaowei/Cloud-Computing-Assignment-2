08d05beff3e82ec18d9aff41d7b995c72c33652b
Zhuowei(Alvin) Zhao <alvinzhaowei@gmail.com>
2016-05-07 23:18:11 +1000

    Update sentiment.py
    remove json import as we dont need it
    change remove url regular expression
    change the tolower order
    return value type is now integer

--- sentiment.py
+++ sentiment.py
@@ -1,13 +1,13 @@
 # coding: utf-8
 from nltk import tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import json
+
 import nltk
 import re
 
 
 re_at = re.compile(r'@[\w]+')  # a regular expression which match all the string which starts with @
-re_http = re.compile(r'http.*')  # a regular expression which match all the url which starts with http
+re_http = re.compile(r'https?(:|;|.)?/+[\w./?]+')  # a regular expression which match all the url which starts with http
 re_hash_tag = re.compile(r'#[a-zA-Z]+')  # match all the string starts with ‘#’
 re_lower = re.compile(r'a-z')  # a regular expression which match all the lower case
 list_of_corpus = set(nltk.corpus.words.words())  # put all the English words included in NLTK into a set
@@ -18,10 +18,10 @@ english_stop_words = set(nltk.corpus.stopwords.words('english'))
 
 # preprocess single line tweet
 def preprocess(tweet):
+    remove_hashtag_tweet = remove_hashtag_tweet.lower()  # lower case each line with out string start with '#'
     remove_at_tweet = re.sub(re_at, "", tweet).strip()  # each line remove at
     remove_http_tweet = re.sub(re_http, "", remove_at_tweet).strip()  # each line remove http
     remove_hashtag_tweet = re.sub(re_hash_tag, "", remove_http_tweet).strip()  # remove the string start with '#' for each line
-    remove_hashtag_tweet = remove_hashtag_tweet.lower()  # lower case each line with out string start with '#'
     sectences_list = tokenize.sent_tokenize(remove_hashtag_tweet)  # put the segment result into sentence list
     return sectences_list
 
@@ -33,7 +33,7 @@ def sentiment_single_line(line):
     tweet_after_process = preprocess(line)  # a list which contain a list
 
     if tweet_after_process==[]:
-        return "0"
+        return 0
     else:
         for sentence in tweet_after_process:
             ss = sid.polarity_scores(sentence)
@@ -41,11 +41,11 @@ def sentiment_single_line(line):
             tweet_length += len(sentence)
             total_sum = total_sum / tweet_length
             if total_sum < -0.03:
-                return "-1"
+                return -1
             elif total_sum > 0.03:
-                return "1"
+                return 1
             else:
-                return "0"
+                return 0
 
 
 #test
